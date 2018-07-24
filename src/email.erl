54da81f0dbcb3c4e93087f911830185e17b65383
Spoowy <erwin.flaming@gmail.com>
2018-07-24 14:33:24 +0200

    Translating feature

--- src/email.erl
+++ src/email.erl
@@ -4,29 +4,26 @@
 -include_lib("nitro/include/nitro.hrl").
 -include_lib("n2o/include/wf.hrl").
 -include_lib("web/include/users.hrl").
+-include_lib("web/include/lang_shortcuts.hrl").
+% -include_lib("kvs/include/user.hrl").
 -compile(export_all).
 -export(?API).
 
 registration_data(Props, email, Ori)->
   Email = email_prop(Props, email),
-  
-  Ori#user{ display_name = proplists:get_value(<<"display_name">>, Props, Email),
+  Ori#user{ display_name = Email,
             email = Email,
-            names    = proplists:get_value(<<"first_name">>, Props,[]),
-            surnames = proplists:get_value(<<"last_name">>,  Props,[]),
             register_date = os:timestamp(),
             tokens = avz:update({email,Email},Ori#user.tokens),
-            status = proplists:get_value(<<"status">>, Props, ok),
-            type   = proplists:get_value(<<"type">>, Props,[]),
+            status = ok,
             password = avz:sha(proplists:get_value(<<"password">>,Props))}.
 
 index(K) -> wf:to_binary(K).
-email_prop(Props, _) -> proplists:get_value(<<"email">>, Props).
+email_prop(Props, _) -> binary_to_list(proplists:get_value(<<"email">>, Props)).
 
-login_button() -> #button{id=login, body= <<"Sign in">>, postback={email, loginemail}, source=[user,pass]}.
-event({email,loginemail}) -> avz:login(email, [{<<"email">>, wf:q(user)}, {<<"password">>, wf:q(pass)}]);
+login_button() -> #button{id=login, body=?_("Sign in"), postback={email, loginemail}, source=[user,pass]}.
+event({email,loginemail}) -> avz:login(email, [{<<"email">>, list_to_binary(wf:q(user))}, {<<"password">>, wf:q(pass)}]);
 event(_) -> ok.
 api_event(_,_,_) -> ok.
 callback() -> ok.
 sdk() -> [].
-
